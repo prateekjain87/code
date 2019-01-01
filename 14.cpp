1617a68adf02fd3d4a5087bce955aeb8b269bc62
prateekjain87 <akijain058@gmail.com>
2019-01-01 22:26:11 +0530

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#define pi 3.14
+class volume
+{
+  public:
+  int volum(int x)
+  {
+    return x*x*x;
+  }
+  int volum(float r)
+  {
+    return (4*pi*r*r*r)/3);
+  }
+  int volum(int h,float r)
+  {
+    return (pi*r*r*h);
+  }
+};
+int main()
+{
+ int ch;
+ int  side,height;
+ float radius;
+ volume v1;
+ cout<<"Enter the body whose volume is to be calculated:";
+ cout<<endl<<"1.Cube"<<endl<<"2.Cylinder"<<endl<<"3.Sphere"<<endl;
+ cin ch;
+ if(ch==1)
+ {
+ cout<<"Volume Of Cube is:"<<v1.volum()
+ }
+}
+
